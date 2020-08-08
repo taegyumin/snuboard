7ecf66b83cab991774e41951d9e206bf200a5d61
Taegyu Min <mtg821@gmail.com>
2020-08-08 20:28:33 +0900

    chore(crawler): move lambda_function.py from root to /crawler

---
+++ crawler/lambda_function.py
@@ -0,0 +1,25 @@
+import json
+from crawler.common import crawl
+from dynamoDB import insert, scan
+import boto3
+
+def lambda_handler(event=None, context=None):
+    notices = []
+    # answer = []
+
+    dynamoDB = boto3.resource('dynamodb')
+    table = dynamoDB.Table('boards')
+    response = table.scan()
+    boards = response['Items']
+
+    board_ids = []
+    for board in boards:
+        board_ids.append(board['id'])
+    board_ids = sorted(board_ids)
+
+    for board_id in board_ids:
+        notices += (crawl(board_id=str(board_id)))
+
+    insert('notices', items=notices)
+
+    return notices

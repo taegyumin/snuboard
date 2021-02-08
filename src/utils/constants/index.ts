9288f8b8d000fda91ba21714bc8fdb2fa9af1258
Taegyu Min <mtg821@gmail.com>
2021-02-08 23:36:06 +0900

    chore: set serverless yml

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,5 +1,18 @@
 import { tables } from './table-names';
 
 const { STAGE } = process.env as { STAGE: string };
+const isDevEnv = STAGE === 'dev';
+const { DEV_HTTP_PORT, DEV_LAMBDA_PORT } = process.env;
 
-export const appConfig = { tables, stage: STAGE };
+export const appConfig = {
+  tables,
+  secretKeys: 'snuboard-tyler',
+  stage: STAGE,
+  isDevEnv,
+  httpEndpoint:
+    isDevEnv || !process.env.HTTP_ENDPOINT
+      ? `http://localhost:${DEV_HTTP_PORT}/dev`
+      : process.env.HTTP_ENDPOINT,
+  devLambdaEndpoint: `http://localhost:${DEV_LAMBDA_PORT}`,
+  serviceLaunchedAt: '2021-01-01',
+};
